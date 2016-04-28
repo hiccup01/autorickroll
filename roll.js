ccd7634b7ad10d0bd5d23457e4d01ae1cb6304d7
hiccup01 <hiccup@hiccup01.com>
2016-04-28 15:55:11 +1200

    Revamp

---
+++ roll.js
@@ -0,0 +1,10 @@
+//function size() {
+//document.getElementById('video').style.width = (window.innerWidth
+//|| document.documentElement.clientWidth
+//|| document.body.clientWidth + "px");
+//}
+
+//window.addEventListener("load", function load(event){
+//    window.removeEventListener("load", load, false); //remove listener, no longer needed
+//    size();  
+//},false);
